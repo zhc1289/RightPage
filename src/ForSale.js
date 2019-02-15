d41c92cf600e942b548a70cfedd38c886b63bf7f
zhi <zhc1289@gmail.com>
2019-02-14 22:19:15 -0500

    first ForSale commit

--- src/ForSale.js
+++ src/ForSale.js
@@ -4,6 +4,11 @@ function ForSale () {
     return(
         <div>
             <h1>ForSale</h1>
+            <ul>
+                <li>Baby Karn</li>
+                <li>Elesh Norn</li>
+                <li>Dark Confidant</li>
+            </ul>
         </div>
     )
 }
